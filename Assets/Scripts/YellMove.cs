334b297521f7a1d3f0cfcf511e089111224c9f50
KNTy1018 <soramametaro1018@gmail.com>
2025-05-29 12:45:20 +0900

    [update]Yellの動きを変更した

--- Assets/Scripts/YellMove.cs
+++ Assets/Scripts/YellMove.cs
@@ -11,17 +11,18 @@ public class YellMove : MonoBehaviour
     [Tooltip("スピード倍率を設定してください")]
     public float speed = 1.0f;
 
-    private float theta;
+    private float theta, range;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         theta = Random.Range(0, 2 * Mathf.PI);
+        range = Random.Range(-1, 1) * Mathf.PI / 12;
         transform.position = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta)) * radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Mathf.Cos(theta + Mathf.PI), Mathf.Sin(theta + Mathf.PI)) * 0.1f * speed;
+        transform.position += new Vector3(Mathf.Cos(theta + Mathf.PI + range), Mathf.Sin(theta + Mathf.PI + range)) * 0.1f * speed;
     }
 }
